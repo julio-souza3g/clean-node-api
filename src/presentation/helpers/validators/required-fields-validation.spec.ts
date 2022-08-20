cffb431870b96e86a0e7928fa79ef12e8ca3844c
jfdsouza988 <juliosouza.rs@gmail.com>
2022-08-19 23:14:07 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -5,7 +5,7 @@ const makeSut = (): RequiredFieldValidation => {
   return new RequiredFieldValidation('field')
 }
 
-describe('', () => {
+describe('RequiredField Validation', () => {
   it('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
